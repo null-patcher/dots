d009c6e7ff4497379c4d2d8f19a67de02613c736
Robert Piringiu <robert.piringiu@emag.ro>
2025-05-27 10:34:45 +0300

    Added clipboard manager install

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt update
-sudo apt install zsh sway swaylock swayidle waybar neovim alacritty wl-clipboard clipman grim grimshot fzf
-git clone --depth 1 https://github.com/wbthomason/packer.nvim \
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
+sudo dnf copr enable wiiznokes/cosmic-applets-unofficial -y 
+sudo dnf install cosmic-ext-applet-clipboard-manager dnf-plugins-core -y
+sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo -y
+sudo dnf install brave-browser -y
